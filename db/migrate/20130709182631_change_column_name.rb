4f3f69a75651dd4d5b5b58c0a9860c9e413af091
cokeefe <chaemobile@gmail.com>
2013-07-11 07:23:48 -0400

    remove file

--- db/migrate/20130709182631_change_column_name.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration
-  def change
-      rename_column :lists, :list_status, :status
-      rename_column :lists, :name, :description
-      rename_column :tasks, :task_status, :status
-  end
-end
